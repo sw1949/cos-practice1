927da42464eb4af643a838cd99bf69c28f601280
Sewook Hwang <riser1949@kentech.ac.kr>
2026-04-08 12:08:19 +0000

    5

---
+++ 5/addition.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+int uadd_ok(unsigned a, unsigned b) {
+    return 0;
+}
+
+int tadd_ok(int a, int b) {
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int a, b;
+    unsigned x, y;
+
+    if (argc != 3) {
+        cout << "You should insert two numbers in a hexadecimal way" << endl;
+        return 0;
+    }
+
+    a = (int)strtol(argv[1], NULL, 16);
+    b = (int)strtol(argv[2], NULL, 16);
+
+    x = (unsigned)a;
+    y = (unsigned)b;
+
+    cout << "Unsigned Addition ===" << endl;
+    if (uadd_ok(x, y)) {
+        cout << "No overflow" << endl;
+    } else {
+        cout << "Overflow" << endl;
+    }
+
+    cout << "Signed Addition ===" << endl;
+    if (tadd_ok(a, b)) {
+        cout << "No overflow" << endl;
+    } else {
+        cout << "Overflow" << endl;
+    }
+
+    return 0;
+}
